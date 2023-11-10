8e55a77cc80410170be66d1d54f530ec834e657e
Peter Winskill <p.winskill@imperial.ac.uk>
2023-11-10 11:05:11 +0000

    Add in cluster lapply call for testing cluster, orderly2 and renv

--- mission_control.R
+++ mission_control.R
@@ -14,9 +14,10 @@ ctx <- context::context_save("contexts")
 config <- didehpc::didehpc_config()
 obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages("mrc-ide/orderly2")
+obj$install_packages("mrc-ide/peeps")
 obj$install_packages("dplyr")
 
-lapply(isos, function(iso){
+obj$lapply(isos, function(iso){
   orderly2::orderly_run(
     name = "adjust_rates",
     parameters = list(
